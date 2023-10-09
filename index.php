4ce91403374aa58bf2eb234e39733aed8fc63e40
hegeduscili <hegeduscecilia@gmail.com>
2023-10-09 13:28:56 +0200

    navigacio_elkeszitese

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+  rel="stylesheet"
+/>
+
+    <title>Napihírek.hu</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav>
+        <h1>napihírek.hu</h1>
+        <div class="altalanos">
+            <a href="#">Kezdőlap</a>
+            <a href="#">Mai hírek</a>
+            <a href="#">Legtöbbször kattintott</a>
+        </div>
+        <div class="felhasznalo">
+            <a href="login.php"><i class="fa-solid fa-user"></i>Bejelentkezés</a>
+            <a href="register.php">Regisztráció</a>
+        </div>
+    </nav>
+</body>
+
+</html>
